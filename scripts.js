f90d0f36d07853d5ff2e4d53c34df1c7c1bf0b87
David van den Bor <david@davidvandenbor.nl>
2020-12-30 23:34:29 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -31,15 +31,3 @@ document.querySelectorAll('.box')
 
 	document.querySelectorAll('.circle')
 	.forEach(item => { observer.observe(item) });
-
-//add this js script into the web page,
-//you want reload once after first load
-window.onload = function() {
-    //considering there aren't any hashes in the urls already
-    if(!window.location.hash) {
-        //setting window location
-        window.location = window.location + '#loaded';
-        //using reload() method to reload web page
-        window.location.reload();
-    }
-}
