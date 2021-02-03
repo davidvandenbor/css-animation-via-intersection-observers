0c614b3c3735f7de1e4efbb571338a5774110746
David van den Bor <david@davidvandenbor.nl>
2021-02-03 16:25:55 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,22 +1,19 @@
 
 const options = {
 	root: null, // use the document's viewport as the container
-	rootMargin: '10px', // % or px - offsets added to each side of the intersection 
-	threshold: 0.4 // percentage of the target element which is visible
+	rootMargin: '0px', // % or px - offsets added to each side of the intersection 
+	threshold:.4// percentage of the target element which is visible
 }
 
 /** YOU CAN LOOP THROUGH THE CALLBACK
  		WITH "FOR EACH()" AND "FOR() LOOPS":
 */ 
 
-/**  THE FOR() LOOP WAY!
+/**  THE FOR..OF() LOOP WAY!
 let callback = (items) => { 
 var item = callback;
 
 for (item of items) {
-// If item (box) is visible - according to the params set in "options"
-// then adds classes to the ".box" items
-// otherwise removes classes from the ".box" items
  		if(item.isIntersecting) {
 			item.target.classList.add('zoom');
  			item.target.classList.add('rotate');
@@ -34,13 +31,10 @@ let callback = (items) => {
 		// If item (box) is visible - according to the params set in `options`
 		// then adds classes to the ".box" items
 		// otherwise removes classes from the ".box" items
-		if(item.isIntersecting) {
-			item.target.classList.add('zoom');
-			item.target.classList.add('rotate');
-		} else {
-			item.target.classList.remove('zoom');	
-			item.target.classList.remove('rotate');	
-		}
+		if(item.isIntersecting) {	
+			document.querySelector("#circle").classList.toggle('zoom');
+			document.querySelector("#box").classList.toggle('rotate');
+		} 
 	});
 }
 
@@ -51,11 +45,12 @@ let callback = (items) => {
 
 let observer = new IntersectionObserver(callback, options);
 
-// Get all the ".box" and ".circle" items 
-// from the DOM and attach the observer to them:
+// Get all the "#box" and "#circle" items 
+// from the DOM and attach the observer to them!
 
-document.querySelectorAll('.box')
+document.querySelectorAll('#box')
 	.forEach(item => { observer.observe(item) });
 
-	document.querySelectorAll('.circle')
+	document.querySelectorAll('#circle')
 	.forEach(item => { observer.observe(item) });
+
